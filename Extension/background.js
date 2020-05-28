4b433f681a3d6d9b86b7aed4e2230db2879defec
Luke Lynch <tech.luk3@gmail.com>
2020-05-28 11:38:06 +1000

    removed misplaced function causing error

--- Extension/background.js
+++ Extension/background.js
@@ -19,12 +19,3 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 });
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
