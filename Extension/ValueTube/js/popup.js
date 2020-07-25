308528eaafe3d5c0cadff8fac88a8c765210df06
Luke Lynch <tech.luk3@gmail.com>
2020-07-25 11:39:10 +1000

    removed change colour

--- Extension/ValueTube/js/popup.js
+++ Extension/ValueTube/js/popup.js
@@ -1,4 +1,4 @@
-let changeColor = document.getElementById('changeColor');
+let curatorInput = document.getElementById('curatorInput');
 let videoID;
 
 //get videoID
@@ -6,22 +6,8 @@ chrome.tabs.query({active: true, currentWindow: true}, ([currentTab]) => {
     let url_string = currentTab.url;
     let url = new URL(url_string);
     videoID = url.searchParams.get("v");
+    //TODO: Add check to add ValueTube Section
 })
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {
-              code: 'let ybody = document.getElementById("content");' +
-              'ybody.style.backgroundColor = "' + color + '";'
-        });
-    });
-};
 
