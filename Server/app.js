1e4ccd5b0148aef3a72e0a17b01fdde42cb5904e
Luke Lynch <Tech.Luk3@gmail.com>
2020-08-18 22:37:30 +1000

    Removed Server files and merging branch with development. Moving to standalone heroku repository

--- Server/app.js
+++
@@ -1,9 +0,0 @@
-// app.js
-var express = require('express');
-var app = express();
-var db = require('./db');
-module.exports = app;
-
-var UserController = require('./user/UserController')
-app.use('/users', UserController); // <--- This is the url ending that is called by the extension
-module.exports = app;
--- Server/db.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-// TODO: Change Database to ValueTube
-mongoose.connect('mongodb+srv://ValueTube:3M60BDyt6Gm@valuetube.jnanb.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
-
-console.log(mongoose.connection.readyState);
