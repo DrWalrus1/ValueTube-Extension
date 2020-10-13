5291d00ab8fdf040190788238619fde6a953667c
Luke Lynch <tech.luk3@gmail.com>
2020-10-13 14:40:51 +1100

    Resolved Issue #1. Curator Mode settings are now synced across all tabs and windows

--- Extension/js/popup.js
+++ Extension/js/popup.js
@@ -17,24 +17,11 @@ if (localStorage.getItem("Whitelist") == "true") {
 curatorInput.onchange = function() {
     if (curatorInput.checked == true) {
         localStorage.setItem("VTCuratorMode", "true");
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.executeScript(
-                tabs[0].id,
-                {
-                    code: 'createCuratorDiv()'
-                })
-        });
+        modifyYoutubeTabsInWindows();
     } else {
-        localStorage.setItem("VTCuratorMode", "false");
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.executeScript(
-                tabs[0].id,
-                {
-                    code: 'removeCuratorDiv();'
-                })
-        });
-        
+        localStorage.setItem("VTCuratorMode", "false");   
     }
+    modifyYoutubeTabsInWindows();
 };
 
 blackListSwitch.onchange = function() {
@@ -60,25 +47,54 @@ whiteListSwitch.onchange = function() {
     save_options();
 }
 
-window.addEventListener('storage', function() {
-    if (localStorage.getItem("VTCuratorMode") == "true") {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.executeScript(
-                tabs[0].id,
-                {
-                    code: 'createCuratorDiv()'
-                })
-        });
+/**
+ * Triggers the extension to search all tabs in
+ * all windows to create or remove the curator input
+ * div.
+ */
+function modifyYoutubeTabsInWindows() {
+    //let expression = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.youtube.com*(:[0-9]{1,5})?(\/.*)?$";
+    chrome.windows.getAll({"populate": true, "windowTypes" :["normal"]}, function(windowArray) {
+        // Iterate through windows
+        for (let i = 0; i < windowArray.length; i++) {
+            // Iterate through tabs in window
+            for (let x = 0; x < windowArray[i].tabs.length; x++) {
+                if (windowArray[i].tabs[x].url.includes("youtube.com")) {
+                    if (localStorage.getItem("VTCuratorMode") == "true") {
+                        SetCuratorDiv(true, windowArray[i].tabs[x].id);
+                    } else {
+                        SetCuratorDiv(false, windowArray[i].tabs[x].id);
+                    }
+                }
+            }
+        }
+    })
+}
+
+/**
+ * 
+ * @param {Boolean} state 
+ * @param {Number} tabID
+ */
+function SetCuratorDiv(state, tabID) {
+    if (state) {
+        chrome.tabs.executeScript(
+            tabID,
+            {
+                code : "createCuratorDiv();"
+            }
+            
+        );
     } else {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.executeScript(
-                tabs[0].id,
-                {
-                    code: 'removeCuratorDiv();'
-                })
-        });
+        chrome.tabs.executeScript(
+            tabID,
+            {
+                code : "removeCuratorDiv();"
+            }
+            
+        );
     }
-});
+}
 
 // chrome.storage.local.onChange.addListener(function(changes, storageName){
 //     chrome.browserAction.setBadgeText({"text": "1"});
