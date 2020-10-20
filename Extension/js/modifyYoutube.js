ba808658d0710310e56546d8b188c575625229e9
sam007mac <sammacdonald007@gmail.com>
2020-10-20 22:46:46 +1100

    Subscription feed filtering

--- Extension/js/modifyYoutube.js
+++ Extension/js/modifyYoutube.js
@@ -25,8 +25,8 @@ const page = {
     SEARCH : "https://www.youtube.com/results",
     VIDEO : "https://www.youtube.com/watch",
     CHANNEL : ["https://www.youtube.com/c", "https://www.youtube.com/user"], // FIXME: Multiple urls
-    PLAYLIST : "https://www.youtube.com/playlist",
-    MIX : "https://www.youtube.com/watch?v=&list="
+    PLAYLIST_PAGE : "https://www.youtube.com/playlist",
+    PLAYLIST : "&list="
 };
 const windowMessages = {
     SendCurator : "SubmitVT",
@@ -79,7 +79,7 @@ function OnPageChange() {
 
                 GetRecommendationFeedIDs();
 
-                if ( (window.location.href).includes("&list=")) {
+                if ( (window.location.href).includes(page.PLAYLIST)) {
                     GetPlaylistIDs();
                 }
 
@@ -92,8 +92,10 @@ function OnPageChange() {
                 } else {
                     console.log(FilterChannelHomePage());
                 }
-            } else if ( (window.location.href).includes(page.PLAYLIST)) {
+            } else if ( (window.location.href).includes(page.PLAYLIST_PAGE)) {
                 GetPlaylistPageIDs();
+            } else if ( (window.location.href).includes(page.SUBSCRIPTIONS)) {
+                GetSubscriptionIDs();
             }
     }
 }
@@ -297,6 +299,49 @@ function FilterPlaylistPage(obj) {
 }
 
 // ------------------ END PLAYLIST PAGE FUNCTION ------------------
+
+//  ------------------ SUBSCRIPTIONS FUNCTIONS ------------------
+
+function GetSubscriptionIDs() {
+    console.log("Running VT Subscription Filter")
+    const SubscriptionSection = document.getElementsByTagName("ytd-section-list-renderer")[0];
+    
+    //list view
+    let section = SubscriptionSection.getElementsByTagName("ytd-video-renderer");
+    for (let elem of section) {
+        let link = elem.getElementsByTagName("a")[0].getAttribute('href');
+        let videoID = getVideoID(new URL(link, "https://www.youtube.com"));
+        FilterSubscriptionPage({"vID": videoID, "videoObject": elem});
+    };
+
+    //grid view
+    section = SubscriptionSection.getElementsByTagName("ytd-grid-video-renderer");
+    
+    for (let elem of section) {
+        let link = elem.getElementsByTagName("a")[0].getAttribute('href');
+        let videoID = getVideoID(new URL(link, "https://www.youtube.com"));
+        FilterSubscriptionPage({"vID": videoID, "videoObject": elem});
+    };
+
+    let observer = new MutationObserver(mutations => {
+        for(let mutation of mutations) {
+             for(let addedNode of mutation.addedNodes) {
+                 if (addedNode.tagName === "YTD-GRID-VIDEO-RENDERER" || addedNode.tagName === "YTD-VIDEO-RENDERER") {                   
+                    let link = addedNode.getElementsByTagName("a")[0];
+                    let videoID = getVideoID(new URL(link, "https://www.youtube.com"));
+                    FilterSubscriptionPage({"vID": videoID, "videoObject": addedNode});
+                  }
+              }
+         }
+     });
+     observer.observe(SubscriptionSection, { childList: true, subtree: true });
+}
+
+function FilterSubscriptionPage(obj) {
+    console.log(obj);
+}
+
+// ------------------ END SUBSCRIPTIONS PAGE FUNCTION ------------------
 
 //  ------------------ RECOMMENDATION FEED FUNCTIONS ------------------
 
