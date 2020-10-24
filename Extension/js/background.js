e3cd1dfa6126b361f180f15811008925b5be5572
Luke Lynch <tech.luk3@gmail.com>
2020-10-24 12:39:05 +1100

    Added Extension Marketing Screenshots. Removed declarativeContent permission from manifest as it wasn't being used.

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -27,15 +27,6 @@ chrome.runtime.onInstalled.addListener(function(details) {
     }
   });
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-
 
   if (details.reason == "install") {
     // this logic executes
