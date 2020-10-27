792b935a580bdf69814f4aeb006d87c6f9fb3f30
Luke Lynch <Tech.Luk3@gmail.com>
2020-10-27 21:33:48 +1100

    Moved curator mode settings to developer options

--- Extension/js/popup.js
+++ Extension/js/popup.js
@@ -1,10 +1,6 @@
-let curatorInput = document.getElementById('curatorInput');
 let blackListSwitch = document.getElementById("bListSwitch");
 let whiteListSwitch = document.getElementById("wListSwitch");
 
-if (localStorage.getItem("VTCuratorMode") == "true") {
-	curatorInput.checked = true;
-}
 
 if (localStorage.getItem("Blacklist") == "true") {
 	blackListSwitch.checked = true;
@@ -14,15 +10,6 @@ if (localStorage.getItem("Whitelist") == "true") {
 	whiteListSwitch.checked = true;
 }
 
-curatorInput.onchange = function() {
-	if (curatorInput.checked == true) {
-		localStorage.setItem("VTCuratorMode", "true");
-		modifyYoutubeTabsInWindows();
-	} else {
-		localStorage.setItem("VTCuratorMode", "false");   
-	}
-	modifyYoutubeTabsInWindows();
-};
 
 blackListSwitch.onchange = function() {
 	if (blackListSwitch.checked == true) {
@@ -47,53 +34,7 @@ whiteListSwitch.onchange = function() {
 	save_options();
 }
 
-/**
- * Triggers the extension to search all tabs in
- * all windows to create or remove the curator input
- * div.
- */
-function modifyYoutubeTabsInWindows() {
-	chrome.windows.getAll({"populate": true, "windowTypes" :["normal"]}, function(windowArray) {
-		// Iterate through windows
-		for (let i = 0; i < windowArray.length; i++) {
-			// Iterate through tabs in window
-			for (let x = 0; x < windowArray[i].tabs.length; x++) {
-				if (windowArray[i].tabs[x].url.includes("youtube.com")) {
-					if (localStorage.getItem("VTCuratorMode") == "true") {
-						SetCuratorDiv(true, windowArray[i].tabs[x].id);
-					} else {
-						SetCuratorDiv(false, windowArray[i].tabs[x].id);
-					}
-				}
-			}
-		}
-	})
-}
 
-/**
- * 
- * @param {Boolean} state 
- * @param {Number} tabID
- */
-function SetCuratorDiv(state, tabID) {
-	if (state) {
-		chrome.tabs.executeScript(
-			tabID,
-			{
-				code : "createCuratorDiv();"
-			}
-			
-		);
-	} else {
-		chrome.tabs.executeScript(
-			tabID,
-			{
-				code : "removeCuratorDiv();"
-			}
-			
-		);
-	}
-}
 
 // chrome.storage.local.onChange.addListener(function(changes, storageName){
 //     chrome.browserAction.setBadgeText({"text": "1"});
