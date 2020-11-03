ddc3e979b7f58cbafdaa44b2a78bf6adb7e911cc
Luke Lynch <tech.luk3@gmail.com>
2020-11-03 17:19:17 +1100

    Blacklist now successfully shows youtubers that have been searched for. The name must match though.

--- Extension/js/blacklist.js
+++ Extension/js/blacklist.js
@@ -1,3 +1,5 @@
+let input = document.getElementById("blackListInput");
+let resultsArea = document.getElementById("resultsArea");
 addButton();
 
 function addButton() {
@@ -8,9 +10,10 @@ function addButton() {
     button.id = "blackListBtn";
     button.className = "btn btn-primary btn-sm";
     button.innerText = "Search";
+    button.type = "submit";
     button.onclick = function () {
         button.style += "background-color:blue;";
-        console.log("hello");
+        searchYoutuber(input.value);
     }
     buttonDiv.appendChild(button);
 }
@@ -20,20 +23,55 @@ function createOptionDiv(name, imageSrc) {
     div.onclick = function() {
         this.style = 'background-color:dodgerblue;color: white;';
     }
-    div.tabIndex = "-1";
+    div.tabIndex = "0";
     let avatar = document.createElement("img");
     avatar.src = imageSrc;
     avatar.className = "avatar";
 
     let span = document.createElement("span");
     span.style = "display:inline-block;";
-    let channelName = document.createElement("p");
-    channelName.innerHTML = name;
-    span.innerHTML = channelName;
+    let channelName = document.createElement("h5");
+    channelName.innerText = name;
+    span.appendChild(channelName);
+    div.appendChild(avatar);
+    div.appendChild(span);
+    return div;
 }
 
+/**
+ * 
+ * @param {String} name 
+ */
 async function searchYoutuber(name) {
+    name = name.replaceAll(" ", "");
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://api.valuetube.net/filter/youtuber?channelName=' + name);
+    request.setRequestHeader("Content-Type", "application/json");
+  	request.send();
 
+  	request.onload = function() {
+		if (request.status != 200) { // analyze HTTP status of the response
+			return false; // e.g. 404: Not Found
+		} else { // show the result
+		// TODO: Error Handling
+			return true; // response is the server
+		}
+  	}
+  
+	request.onreadystatechange = async function() {
+		if (request.readyState === 4) {
+			let response = JSON.parse(request.response);
+			if (response) {
+                let imgURL = await getImage(response.thumbnails.default.url)
+                let channelTitle = response.title;
+                let newOption = createOptionDiv(channelTitle, imgURL)
+                resultsArea.appendChild(newOption);
+				// console.log(response);
+			} else {
+				console.error("Something went wrong. Please try again");
+			}
+		}
+	}
 }
 
 async function getImage(url) {
@@ -46,3 +84,9 @@ async function getImage(url) {
 	  		return outside;
 		});
 }
+
+input.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        searchYoutuber(this.value);
+    }
+})
